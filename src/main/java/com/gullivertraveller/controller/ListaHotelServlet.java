12998cae414571027c6292b3fba47df7a4176ef2
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2021-09-26 23:05:13 -0300

    Adiciona conteúdo ao servlet

--- src/main/java/com/gullivertraveller/controller/ListaHotelServlet.java
+++ src/main/java/com/gullivertraveller/controller/ListaHotelServlet.java
@@ -21,10 +21,28 @@ public class ListaHotelServlet extends HttpServlet {
 		hoteis.add(new Hotel(
 				"Copacabana Palace",
 				"Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ, CEP: 22021-001",
-				128.0,
+				500.0,
 				5
 		));
-		
+		hoteis.add(new Hotel(
+				"Windsor Leme",
+				"Av. Atlântica, 656 - Leme, Rio de Janeiro - RJ, CEP: 22010-000",
+				300.0,
+				5
+		));
+		hoteis.add(new Hotel(
+				"Fasano Ipanema",
+				"Av. Vieira Souto, 80 - Ipanema, Rio de Janeiro - RJ, CEP: 22420-002",
+				450.0,
+				5
+		));
+		hoteis.add(new Hotel(
+				"Hotel Nacional",
+				"Av. Niemeyer, 769 - São Conrado, Rio de Janeiro - RJ, 22450-221",
+				650.0,
+				5
+		));
+		request.setAttribute("Hoteis", hoteis);
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/hoteis.jsp");
 		dispatcher.forward(request, response);
 	}
